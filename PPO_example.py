cae12a43fce5afc31b9c3baf133b32fc17e7e94f
kinziro <kazushi@MacBook.local>
2020-06-30 08:07:39 +0900

    自作環境を使うように変更

--- PPO_example.py
+++ PPO_example.py
@@ -9,6 +9,7 @@ import time
 from datetime import datetime
 import pytz
 from OpenGL import GLU
+import pandas as pd
 
 #from stable_baselines3.common.policies import MlpPolicy
 from stable_baselines3.ppo import MlpPolicy
@@ -16,6 +17,8 @@ from stable_baselines3.ppo import MlpPolicy
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 from stable_baselines3 import PPO
 from stable_baselines3.common.cmd_util import make_vec_env
+
+import ex_env.kuka
 
 
 #def make_env(env_name, rank, seed=0):
@@ -35,17 +38,18 @@ from stable_baselines3.common.cmd_util import make_vec_env
 
 
 # 設定項目
-train = False            # 学習を行うかどうか
+train = True            # 学習を行うかどうか
 validation = True       # 評価を行うかどうか
 
 #env_name = 'Pendulum-v0'        # 学習環境(ペンデュラム)
-env_name = 'KukaBulletEnv-v0'        # 学習環境(ペンデュラム)
+#env_name = 'KukaBulletEnv-v0'        # 学習環境(Kuka iiwa)
+env_name = 'KukaEnvExperiment-v0'        # 学習環境(自作環境)
 #env_name = 'MountainCar-v0'        # 学習環境(マウンテンカー)
 #env_name = 'MountainCarContinuous-v0'        # 学習環境(マウンテンカー)
 #env_name = 'CartPole-v1'        # 学習環境(カーポール)
 #env_name = 'RoboschoolHumanoid-v1'        # 学習環境(ヒューマノイド)
-num_cpu = 2                   # 分散処理させる数(CPUのハイパースレッドの全数を上限が目安)
-total_timesteps = 1*(10**3)     # 学習を行うタイムステップ数
+num_cpu = 8                   # 分散処理させる数(CPUのハイパースレッドの全数を上限が目安)
+total_timesteps = 1*(10**7)     # 学習を行うタイムステップ数
 #total_timesteps = 4*(10**5)     # 学習を行うタイムステップ数
 
 #ori_env = gym.make(env_name)
@@ -80,38 +84,47 @@ if train:
     '''
 endtime = datetime.now(pytz.timezone('Asia/Tokyo')).strftime("%Y/%m/%d %H:%M:%S")
 
+wrapping = True
 
 # 学習結果の確認
 if validation:
     model = PPO.load('{}ppo_model'.format(savedir))
-    from gym import wrappers
-
     env0 = env.envs[0]
-    video_path = '{}video'.format(savedir)
-    wrap_env = wrappers.Monitor(env0, video_path, force=True)
+
+    if wrapping:
+        from gym import wrappers
+
+        video_path = '{}video'.format(savedir)
+        wrap_env = wrappers.Monitor(env0, video_path, force=True)
+
+        obs = wrap_env.reset()
+    else:
+        obs = env0.reset()
 
     done = False
-    obs = wrap_env.reset()
-    #obs = env0.reset()
+    obs_list = []
 
     sum_r = 0
     for step in range(2000):
         if step % 10 == 0: print("step :", step)
         if done:
             time.sleep(1)
-            o = wrap_env.reset()
+            o = wrap_env.reset() if wrapping else env0.reset()
             #o = env0.reset()
             break
 
         action, _states = model.predict(obs)
-        obs, rewards, done, info = wrap_env.step(action)
+        #action = [0, 0, 0]
+        obs, rewards, done, info = wrap_env.step(action) if wrapping else env0.step(action)
         #obs, rewards, done, info = env0.step(action)
+        obs_list.append(obs.tolist())
         sum_r += rewards
 
+    pd.DataFrame(obs_list).to_csv('{}obs.csv'.format(savedir))
     print('reward', sum_r)
     with open('{}reward_sum.txt'.format(savedir), mode='w') as f:
         f.write(str(sum_r))
-    wrap_env.close()
+    if wrapping: wrap_env.close()
     #ori_env.close()
 env.close()
 
