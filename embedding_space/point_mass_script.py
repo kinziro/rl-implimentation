588b471b29be05dda6278fcbed023b336e72abc6
kinziro <kazushi.ninomiya@adminnoMacBook-Pro.local>
2020-11-18 19:10:13 +0900

    YAMLで設定を管理するように変更

--- embedding_space/point_mass_script.py
+++ embedding_space/point_mass_script.py
@@ -12,6 +12,8 @@ from OpenGL import GLU
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import shutil
+import yaml
 
 #from stable_baselines3.ppo import MlpPolicy
 #from a2c_test import A2C
@@ -138,48 +140,56 @@ def plot_embedding_space(model, task_id_list, title, figdir):
     plt.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=0)
     plt.savefig(os.path.join(figdir, f'{title}.png'))
 
+# 設定ファイルの読み込み
+here = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(here, "config.yml")) as f:
+    config = yaml.safe_load(f.read())
 
 # 設定項目
-train = True            # 学習を行うかどうか
-validation = True       # 評価を行うかどうか
-video = False
-val_num = 1
-
 env_name = 'PointMass-v0'        # 学習環境(自作環境)
-#num_cpu = 2                   # 分散処理させる数(CPUのハイパースレッドの全数を上限が目安)
-num_cpu = 16                   # 分散処理させる数(CPUのハイパースレッドの全数を上限が目安)
-device = 'cpu'
-total_timesteps = 2*(10**6)     # 学習を行うタイムステップ数
-#total_timesteps = 4*(10**5)     # 学習を行うタイムステップ数
-
-
-here = os.path.dirname(os.path.abspath(__file__))
-base_savedir = '{}/result/{}/'.format(here, env_name)      # 結果の保存ディレクトリ
-
-starttime = datetime.now(pytz.timezone('Asia/Tokyo')).strftime("%Y/%m/%d %H:%M:%S")
 
 # 学習の実行
-#task_int_list = [0, 1, 2, 3, 4, 5, 6, 7]
-task_int_list = [0, 1, 2, 3]
-#task_int_list = [0, 1]
-task_id_list = np.eye(8)[task_int_list]
-env_kwargs_list = [dict(task_id=task_id) for task_id in task_id_list]
-env = make_vec_env(PointMassEnv, n_envs=num_cpu, env_kwargs_list=env_kwargs_list)
-env.reset()
+task_int_list = config["env"]["task_int_list"]
+max_task_int = config["env"]["max_task_int"]
+task_id_list = np.eye(max_task_int+1)[task_int_list]
 
+base_savedir = '{}/result/{}/'.format(here, env_name)      # 結果の保存ディレクトリ
 savedir = '{}multi_task/'.format(base_savedir)
 logdir = '{}tensorboard_log/'.format(savedir)       # tensorboardのログ保存ディレクトリ
 os.makedirs(savedir, exist_ok=True)
-if train:
-    #policy_kwargs = dict(net_arch=[200, 100])
-    policy_kwargs = dict(net_arch=[50, 50])
-    inference_kwargs = dict(net_arch=[50, 50])
-    embedding_kwargs = dict(net_arch=[50, 50])
-    schedule_fn = get_schedule_fn(7e-4)
-    optimizer_kwargs = dict(lr=schedule_fn(1), alpha=0.99, eps=1e-5, weight_decay=0.001)
-    model = A2C(PolicyNet, env, verbose=1, tensorboard_log=logdir, policy_kwargs=policy_kwargs, 
+
+starttime = datetime.now(pytz.timezone('Asia/Tokyo')).strftime("%Y/%m/%d %H:%M:%S")
+
+# train
+if config["learn"]["flag"]:
+    # 設定ファイルをコピー
+    shutil.copyfile(os.path.join(here, 'config.yml'), os.path.join(savedir, 'config.yml'))
+
+    n_cpu = config["learn"]["n_cpu"]                   # 分散処理させる数(CPUのハイパースレッドの全数を上限が目安)
+    # 環境の構築
+    env_kwargs = config["env"]["env_kwargs"]
+    env_kwargs_list = [dict(task_id=task_id) for task_id in task_id_list]
+    [env_k.update(env_kwargs) for env_k in env_kwargs_list]
+    env = make_vec_env(PointMassEnv, n_envs=n_cpu, env_kwargs_list=env_kwargs_list)
+    env.reset()
+
+    # ネットワークの定義
+    embedding_dim = config["algorithm"]["embedding_dim"]
+    policy_kwargs = config["algorithm"]["policy_kwargs"]
+    inference_kwargs = config["algorithm"]["inference_kwargs"]
+    embedding_kwargs = config["algorithm"]["embedding_kwargs"]
+    optimizer_kwargs = config["algorithm"]["optimizer_kwargs"]
+
+    # 学習条件の定義
+    total_timesteps = config["learn"]["total_timesteps"]     # 学習を行うタイムステップ数
+    device = config["learn"]["device"]
+    n_steps =  config["learn"]["n_steps"]
+    verbose =  config["learn"]["verbose"]
+
+    # モデルの作成
+    model = A2C(PolicyNet, env, verbose=verbose, tensorboard_log=logdir, policy_kwargs=policy_kwargs, 
                 inference_kwargs=inference_kwargs, embedding_kwargs=embedding_kwargs, 
-                optimizer_kwargs=optimizer_kwargs, task_id_list=task_id_list, embedding_dim=2, device=device, n_steps=30)
+                optimizer_kwargs=optimizer_kwargs, task_id_list=task_id_list, embedding_dim=embedding_dim, device=device, n_steps=n_steps)
     #model = A2C(MlpPolicy, env, verbose=1, tensorboard_log=logdir, policy_kwargs=policy_kwargs, 
     #            device=device, n_steps=10)
 
@@ -213,12 +223,17 @@ if train:
 
 
     model.save('{}embedding_model'.format(savedir))
-env.close()
+    env.close()
 endtime = datetime.now(pytz.timezone('Asia/Tokyo')).strftime("%Y/%m/%d %H:%M:%S")
 
 
 # 学習結果の確認
-if validation:
+if config["validation"]["flag"]:
+    # 検証条件
+    device = config["validation"]["device"]
+    video = config["validation"]["flag"]
+    n_val = config["validation"]["n_val"]
+    timestep =  config["validation"]["timestep"]
 
     model = A2C.load('{}embedding_model.zip'.format(savedir), device=device)
     plot_embedding_space(model, task_id_list, 'embedding_space_leaned', figdir=savedir)
@@ -228,11 +243,12 @@ if validation:
         val_savedir = os.path.join(savedir, f'{task_int}')
         os.makedirs(val_savedir, exist_ok=True)
 
-        #env0 = make_vec_env(env_name, n_envs=1).envs[0]
+        env_kwargs = config["env"]["env_kwargs"]
         env_kwargs_list = [dict(task_id=task_id)]
+        [env_k.update(env_kwargs) for env_k in env_kwargs_list]
         env0 = make_vec_env(PointMassEnv, n_envs=1, env_kwargs_list=env_kwargs_list).envs[0]
 
-        wrapping_flags = [False] * val_num
+        wrapping_flags = [False] * n_val
         if video: wrapping_flags[-1] = True
 
         sum_r_list = []
@@ -258,7 +274,7 @@ if validation:
             ac_obs_list = []
 
             sum_r = 0
-            for step in range(50):
+            for step in range(timestep):
                 if step % 10 == 0 and wrapping: print("--step :", step)
                 if done:
                     time.sleep(1)
